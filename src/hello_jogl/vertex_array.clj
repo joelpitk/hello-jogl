4cf526e23a9f436fe21e63e4c1e8248309cb226d
Joel Pitkänen <joel.pitkanen@cs.helsinki.fi>
2014-02-09 21:51:23 +0200

    vertex-array now creates/uses index buffers.

--- src/hello_jogl/vertex_array.clj
+++ src/hello_jogl/vertex_array.clj
@@ -2,33 +2,48 @@
   (:import (java.nio FloatBuffer IntBuffer)
            (com.jogamp.opengl.util FPSAnimator GLBuffers)))
 
-(defn create [gl vertex-positions]
+(defn create [gl geometry]
   (let [vertex-array-objects (int-array 1)
         vertex-buffer-objects (int-array 1)
-        vertex-position-buffer (GLBuffers/newDirectFloatBuffer (float-array vertex-positions))]
+        index-buffer-objects (int-array 1)
+        vertex-buffer-data (GLBuffers/newDirectFloatBuffer (float-array (:vertices geometry)))
+        index-buffer-data (GLBuffers/newDirectIntBuffer (int-array (:indices geometry)))
+        vertex-buffer-size-in-bytes (int (* (count (:vertices geometry)) 4))
+        index-buffer-size-in-bytes (int (* (count (:indices geometry)) 4))]
     (doto gl
       (.glGenVertexArrays 1 (IntBuffer/wrap vertex-array-objects))
       (.glBindVertexArray (first vertex-array-objects))
+
       (.glGenBuffers 1 (IntBuffer/wrap vertex-buffer-objects))
       (.glBindBuffer javax.media.opengl.GL/GL_ARRAY_BUFFER (first vertex-buffer-objects))
-      (.glBufferData javax.media.opengl.GL/GL_ARRAY_BUFFER (int (* (count vertex-positions) 4)) vertex-position-buffer javax.media.opengl.GL/GL_STATIC_DRAW)
+      (.glBufferData javax.media.opengl.GL/GL_ARRAY_BUFFER vertex-buffer-size-in-bytes vertex-buffer-data javax.media.opengl.GL/GL_STATIC_DRAW)
+
+      (.glGenBuffers 1 (IntBuffer/wrap index-buffer-objects))
+      (.glBindBuffer javax.media.opengl.GL/GL_ELEMENT_ARRAY_BUFFER (first index-buffer-objects))
+      (.glBufferData javax.media.opengl.GL/GL_ELEMENT_ARRAY_BUFFER index-buffer-size-in-bytes index-buffer-data javax.media.opengl.GL/GL_STATIC_DRAW)
+
       (.glVertexAttribPointer 0 4 javax.media.opengl.GL/GL_FLOAT false 0 0))
     {:vertex-array-object (first vertex-array-objects)
      :vertex-buffer-object (first vertex-buffer-objects)
-     :vertex-count (/ (count vertex-positions) 4)}))
+     :index-buffer-object (first index-buffer-objects)
+     :vertex-count (count (:indices geometry))}))
 
 (defn draw [gl vertex-array]
   (doto gl
     (.glBindVertexArray (:vertex-array-object vertex-array))
     (.glEnableVertexAttribArray 0)
-    (.glDrawArrays javax.media.opengl.GL/GL_TRIANGLES 0 (:vertex-count vertex-array))
+    ;(.glDrawArrays javax.media.opengl.GL/GL_TRIANGLES 0 (:vertex-count vertex-array))
+    (.glDrawElements javax.media.opengl.GL/GL_TRIANGLES (:vertex-count vertex-array) javax.media.opengl.GL/GL_UNSIGNED_INT 0)
     (.glDisableVertexAttribArray 0)))
 
 (defn delete [gl vertex-array]
-  (let [buffers-to-delete (int-array [(:vertex-buffer-object vertex-array)])
-        arrays-to-delete  (int-array [(:vertex-array-object vertex-array)])]
+  (let [vertex-buffers-to-delete (int-array [(:vertex-buffer-object vertex-array)])
+        index-buffers-to-delete (int-array [(:index-buffer-object vertex-array)])
+        arrays-to-delete (int-array [(:vertex-array-object vertex-array)])]
     (doto gl
       (.glBindBuffer javax.media.opengl.GL/GL_ARRAY_BUFFER 0)
-      (.glDeleteBuffers 1 buffers-to-delete 0)
+      (.glDeleteBuffers 1 vertex-buffers-to-delete 0)
+      (.glBindBuffer javax.media.opengl.GL/GL_ELEMENT_ARRAY_BUFFER 0)
+      (.glDeleteBuffers 1 index-buffers-to-delete 0)
       (.glBindVertexArray 0)
       (.glDeleteVertexArrays 1 arrays-to-delete 0))))
